f07de9d66d42e45d594d0d95b0d12ec6b9048b0f
kristof de spiegeleer <kristof@incubaid.com>
2016-01-10 10:06:22 +0400

    new

--- setup.py
+++ setup.py
@@ -20,12 +20,6 @@ setup(
     description='Pure Python terminal multiplexer.',
     long_description=long_description,
     packages=find_packages('.'),
-    install_requires = [
-        'prompt_toolkit==0.57',
-        'pyte>=0.4.10',
-        'six>=1.9.0',
-        'docopt>=0.6.2',
-    ],
     entry_points={
         'console_scripts': [
             'pymux = pymux.entry_points.run_pymux:run',
